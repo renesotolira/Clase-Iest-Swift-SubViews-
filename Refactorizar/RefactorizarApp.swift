23853d7a89ef76aeca7028819950ded96cbe461d
René <rene.soto01@iest.edu.mx>
2023-09-18 09:32:41 -0600

    Initial Commit

---
+++ Refactorizar/RefactorizarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefactorizarApp.swift
+//  Refactorizar
+//
+//  Created by Rene SL on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RefactorizarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
